5dc48e8660ccdaf898c0b717acb3176cef262539
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2026-03-31 11:45:35 +0100

    Modified pad node

--- src/tempest_embedding/utils/misc.py
+++ src/tempest_embedding/utils/misc.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 DEFAULT_DATASETS = ['CollegeMsg', 'enron', 'TaobaoSmall', 'mooc', 'wikipedia', 'reddit']
-PAD_NODE_ID = 0  # change to -1 later if needed
+PAD_NODE_ID = -1
 
 
 def process_sampling_numbers(num_neighbors, num_layers):
